efbaf2c1337ad88e6397f4800b319ca976f806e6
Jackelinem <irma2212j@gmail.com>
2017-11-23 18:12:03 -0500

    crud tabla propiedad

---
+++ Ventapp/src/main/java/ec/edu/ups/Controller/PropiedadController.java
@@ -0,0 +1,119 @@
+package ec.edu.ups.Controller;
+
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+import javax.inject.Inject;
+
+import ec.edu.ups.Dao.PropiedadDao;
+import ec.edu.ups.Model.Propiedad;
+
+@ManagedBean
+public class PropiedadController {
+	
+	private Propiedad propiedad;
+	private List<Propiedad> listpropiedades;
+	private int id;
+	
+	@Inject
+	private PropiedadDao propiedadDao;
+	
+	
+	@PostConstruct
+	public void init() {
+		propiedad =new Propiedad();
+		loadPropiedades();
+	}
+
+	
+	public String guardar() {
+		System.out.println(toString());
+		
+		propiedadDao.guardar(propiedad);
+		
+		
+		return null;
+	}
+	
+	public String eliminar(int codigo) {
+		System.out.println(toString());
+		
+			try {
+				
+				propiedadDao.eliminar(codigo);
+				loadPropiedades();
+				
+			}
+			catch (Exception e) {
+				System.out.println("EERORRORO");
+				
+			}
+		
+		
+		return"listarPropiedades";
+	}
+	
+	public String loadDatosEditar(int codigo) {
+		
+		propiedad =propiedadDao.leer(codigo);
+		return"EditarPropiedad";
+	}
+	
+
+	
+	
+	
+	
+	
+	
+	
+	public int getId() {
+		return id;
+	}
+
+
+	public void setId(int id) {
+		this.id = id;
+		
+	loadDatosEditar(id);
+	}
+
+
+	public List<Propiedad> getListpropiedades() {
+		return listpropiedades;
+	}
+
+
+	public void setListpropiedades(List<Propiedad> listpropiedades) {
+		this.listpropiedades = listpropiedades;
+	}
+
+
+	
+
+
+	public Propiedad getPropiedad() {
+		return propiedad;
+	}
+
+
+	public void setPropiedad(Propiedad propiedad) {
+		this.propiedad = propiedad;
+	}
+
+	
+	private void loadPropiedades() {
+		
+	listpropiedades=propiedadDao.listadoPropiedades();
+	}
+	
+	
+	
+
+
+
+	
+	
+	
+}
---
+++ Ventapp/src/main/java/ec/edu/ups/Model/Propiedad.java
@@ -0,0 +1,159 @@
+package ec.edu.ups.Model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.Size;
+
+import org.hibernate.validator.constraints.NotEmpty;
+
+
+
+@Entity
+@Table(name="Propiedades")
+public class Propiedad {
+	
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int codigo;
+	
+
+	@Size(min=1, max=50)
+	private String direccion;
+	
+	@NotEmpty
+	private String urlVideo;
+	
+	@Size(min=1, max=50)
+	@NotEmpty
+	private String descripcion;
+	
+	@NotEmpty
+	private String estado;
+	
+	@NotEmpty
+	private String tipo;
+	
+	@NotEmpty
+	private String costo;
+	
+	private float longuitud;
+	private float latitud;
+	
+	private Category category;
+	//private Categoria categoria;
+
+
+	
+
+
+	public int getCodigo() {
+		return codigo;
+	}
+
+
+
+
+	public void setCodigo(int codigo) {
+		this.codigo = codigo;
+	}
+
+
+
+
+	public String getDireccion() {
+		return direccion;
+	}
+
+
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+
+
+	public String getUrlVideo() {
+		return urlVideo;
+	}
+
+
+	public void setUrlVideo(String urlVideo) {
+		this.urlVideo = urlVideo;
+	}
+
+
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+
+
+	public String getEstado() {
+		return estado;
+	}
+
+
+	public void setEstado(String estado) {
+		this.estado = estado;
+	}
+
+
+	public String getTipo() {
+		return tipo;
+	}
+
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+
+
+	public String getCosto() {
+		return costo;
+	}
+
+
+	public void setCosto(String costo) {
+		this.costo = costo;
+	}
+
+
+	public float getLonguitud() {
+		return longuitud;
+	}
+
+
+	public void setLonguitud(float longuitud) {
+		this.longuitud = longuitud;
+	}
+
+
+	public float getLatitud() {
+		return latitud;
+	}
+
+
+	public void setLatitud(float latitud) {
+		this.latitud = latitud;
+	}
+
+
+	public Category getCategory() {
+		return category;
+	}
+
+
+	public void setCategory(Category category) {
+		this.category = category;
+	}
+
+
+	
+	
+}
